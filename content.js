b46e0704b74ab7b8ca50f008fedc0c5d0fdf73b1
ari-schaffel <yaschaffel@gmail.com>
2020-03-19 19:15:28 -0400

    factored out save to csv and converted to save to firebase

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 // content.js
+
 var class_names_for_linkedin ={'name':'inline t-24 t-black t-normal break-words',
                                 'professionalDescription':'mt1 t-18 t-black t-normal',
                                 'location':'t-16 t-black t-normal inline-block',
@@ -6,7 +7,9 @@ var class_names_for_linkedin ={'name':'inline t-24 t-black t-normal break-words'
                                 'companyName':'text-align-left ml2 t-14 t-black t-bold full-width lt-line-clamp lt-line-clamp--multi-line ember-view'
                               };
 
-function printNames(dict){
+const url = 'https://linkedin-data-scrape.firebaseio.com/.json';
+
+function getDataFromeLinkedin(dict){
   data = {}
   //gets more for aboutDescription
   try{
@@ -17,9 +20,7 @@ function printNames(dict){
   var keys = Object.keys(dict);
   for (var i = 0;i<keys.length;i++){
     try{
-      // console.log(class_names_for_linkedin[keys[i]]);console.log(i);
       data[keys[i]] =document.getElementsByClassName(class_names_for_linkedin[keys[i]])[0].textContent.replace(/<br\s*\/?>/gi,' ').trim();
-    // console.log(document.getElementsByClassName(class_names_for_linkedin[keys[i]])[0].textContent)
   }
   catch(err){data[keys[i]]='';console.log("error");console.log(" : ");console.log(keys[i])}
   }
@@ -29,87 +30,47 @@ function printNames(dict){
 
 
 
+async function getData(url){
+  data = await fetch(url)
+  json = await data.json();
+  return json
+}
 
-function convertArrayOfObjectsToCSV(args) {
-            var result, ctr, keys, columnDelimiter, lineDelimiter, data;
 
-            data = args.data || null;
-            if (data == null || !data.length) {
-                return null;
-            }
+async function checkNameinDB(){
+  names = []
+  data = await getData(url)
+  for (const property in data){
+      names.push(`${data[property]['name']}`)
+  }
+  // console.log(names)
+  return names
+}
 
-            columnDelimiter = args.columnDelimiter || '|';
-            lineDelimiter = args.lineDelimiter || '\n';
 
-            keys = Object.keys(data[0]);
-
-            result = '';
-            result += keys.join(columnDelimiter);
-            result += lineDelimiter;
-
-            data.forEach(function(item) {
-                ctr = 0;
-                keys.forEach(function(key) {
-                    if (ctr > 0) result += columnDelimiter;
-
-                    result += item[key];
-                    ctr++;
-                });
-                result += lineDelimiter;
-            });
-
-            return result;
-        }
-
-function downloadCSV(args) {
-                var data, filename, link;
-                var csv = convertArrayOfObjectsToCSV({
-                    data: data_to_csv
-                });
-
-                if (csv == null) return;
-
-                filename = 'export.csv';
-
-                if (!csv.match(/^data:text\/csv/i)) {
-                    csv = 'data:text/csv;charset=utf-8,' + csv;
-                }
-                data = encodeURI(csv);
-                console.log("here1");
-                link = document.createElement('a');
-                link.setAttribute('href', data);
-                link.setAttribute('download', filename);
-                link.click();
-            }
 
 chrome.runtime.onMessage.addListener(
 
 
-  function(request, sender, sendResponse) {
-    if (request.message === "download_data"){
-      console.log("here")
-      data_to_csv = JSON.parse(localStorage.getItem("data_to_save"));
-      console.log(data_to_csv);
-      hope1 = downloadCSV(data_to_csv);
-      localStorage.removeItem('data_to_save');
-      var emptyData = [];
-                                            };
+  async function(request, sender, sendResponse) {
+    console.log('here')
+    names = await checkNameinDB()
+    console.log(names)
     if( request.message === "save_data" )   {
-      var emptyData = [];
-      data1 = printNames(class_names_for_linkedin)
-      data1['url'] =window.location.href;
-      emptyData.push(data1);
-      // console.log(emptyData);
-      console.log(data1)
-      try{
-      oldData = JSON.parse(localStorage.getItem('data_to_save'));
-      oldData.push(data1);
-      localStorage.setItem('data_to_save',JSON.stringify(oldData));
-         }
-      catch(err){console.log("starting here");localStorage.setItem('data_to_save',JSON.stringify(emptyData))}
+      linkedinData = getDataFromeLinkedin(class_names_for_linkedin)
+      linkedinData['url'] =window.location.href;
+      if (names.includes(linkedinData['name'])){alert('Already Saved')}
 
+      else{
+        console.log(data)
+        $.post('https://linkedin-data-scrape.firebaseio.com/.json',   JSON.stringify(linkedinData),   function () {     alert("success");   } )
+        console.log('Data Saved');
+      }
 
-      console.log('Data Saved');
-                                            }
+      
+}
   }
 );
+// https://linkedin-data-scrape.firebaseio.com/
+
+// $.post('https://linkedin-data-scrape.firebaseio.com/.json',   JSON.stringify(params),   function () {     alert("success");   } );
